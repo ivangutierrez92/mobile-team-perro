1777f2deec1e0886baaa79d0a37ecb9b0c06b35b
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-12-07 12:46:16 -0300

    Vuelta Home a su estado inicial en Stack

--- src/Navigation/Stack.jsx
+++ src/Navigation/Stack.jsx
@@ -8,9 +8,9 @@ const StackNav = createNativeStackNavigator();
 export default function Stack() {
   return (
     <StackNav.Navigator>
+      <StackNav.Screen name="Home" component={Home} />
       <StackNav.Screen name="Cities" component={Cities} />
       <StackNav.Screen name="City" component={City} />
-      <StackNav.Screen name="Home" component={Home} />
       <StackNav.Screen name="Hotels" component={Hotels} />
     </StackNav.Navigator>
   )
