27bc65a93b680a910c3a9cd3049b398dfb084b1d
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2022-12-07 09:48:52 -0300

    first commit

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ImageBackground, StyleSheet, Image } from "react-native";
+import React from "react";
 
 export default function Home() {
   return (
     <View>
-      <Text>Home</Text>
+      <ImageBackground
+        source={require("../../assets/img/banner.jpg")}
+        style={styles.banner}
+      >
+        <Text style={styles.text}>
+          If not now, then when? Travel with us to your next adventure
+        </Text>
+      </ImageBackground>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  banner: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  text: {
+    color: "white",
+    fontSize: 40,
+    textAlign: "center",
+  },
+});
