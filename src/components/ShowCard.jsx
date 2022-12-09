631add18fcda0e998d33732f5643e8b5093b1b3f
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2022-12-09 13:26:56 -0300

    Comentarios!

--- src/components/ShowCard.jsx
+++ src/components/ShowCard.jsx
@@ -1,19 +1,100 @@
 import { View, Text, Button, StyleSheet, Image } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import NewComment from "./NewComment";
 
-export default function ShowCard(item) {
+import { useDispatch, useSelector } from "react-redux";
+import reactionsActions from "../redux/actions/reactionsActions";
+import commentsActions from "../redux/actions/commentsActions";
+import Comment from "./Comment";
+import Reaction from "./Reaction";
+
+export default function ShowCard({ item }) {
+  const [showComments, setShowComments] = useState(false);
+  const user = useSelector((store) => store.signIn);
+  let comments = useSelector((store) => store.comments);
+  let reactions = useSelector((store) => store.reactions);
+  const dispatch = useDispatch();
+
+  const { getInicialComments, createComment } = commentsActions;
+  const { toggleReaction } = reactionsActions;
+
+  useEffect(() => {
+    dispatch(
+      getInicialComments({
+        id: item._id,
+        query: { params: { showId: item._id } },
+      })
+    );
+  }, []);
+
+  const toggleComments = () => {
+    setShowComments(!showComments);
+  };
+
+  const onReaction = (name) => {
+    dispatch(
+      toggleReaction({ name, showId: item._id, token: user.token })
+    );
+  };
+
+  const sendComment = (textToSend) => {
+    let headers = { headers: { Authorization: `Bearer ${user.token}` } };
+    let newComment = { comment: textToSend, showId: item._id };
+    dispatch(createComment({ newComment, id: item._id, headers }));
+  };
+
   return (
     <>
       <View style={styles.container}>
-        <Image style={styles.image} source={{ uri: item.item.photo }} />
+        <Image style={styles.image} source={{ uri: item.photo }} />
         <View style={styles.content}>
-          <Text style={styles.title}>{item.item.name}</Text>
+          <Text style={styles.title}>{item.name}</Text>
           <Text style={styles.contentText}>
-            description: {item.item.description}
+            description: {item.description}
           </Text>
         </View>
-        <Button color={"#00539c"} style={styles.button} title="Comments" />
+        <Button
+          color={"#00539c"}
+          style={styles.button}
+          title="Comments"
+          onPress={toggleComments}
+        />
       </View>
+      {showComments && (
+        <>
+          <View>
+            {user.logged && (
+              <NewComment user={user} sendComment={sendComment} />
+            )}
+          </View>
+          <View>
+            {comments[item._id]?.map((comment) => (
+              <Comment
+                key={comment._id}
+                comment={comment}
+                name={comment.userId.name || user.name}
+                isUser={user.id === (comment.userId._id || comment.userId)}
+              />
+            ))}
+          </View>
+        </>
+      )}
+      
+      {reactions[item._id] && (
+        <View style={styles.reactionContainer}>
+          {reactions[item._id].map((reaction) => (
+            <Reaction
+              key={reaction._id}
+              name={reaction.name}
+              icon={reaction.icon}
+              iconBack={reaction.iconBack}
+              reacted={reaction.reacted}
+              count={reaction.userId}
+              onReaction={() => onReaction(reaction.name)}
+            />
+          ))}
+        </View>
+      )}
     </>
   );
 }
